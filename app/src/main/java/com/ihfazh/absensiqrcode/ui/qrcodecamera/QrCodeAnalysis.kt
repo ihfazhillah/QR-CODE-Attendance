919012940d5be2a453841f661d29b81e21c2c071
ihfazhillah <mihfazhillah@gmail.com>
2021-06-05 21:14:25 +0700

    update min sdk

--- app/src/main/java/com/ihfazh/absensiqrcode/ui/qrcodecamera/QrCodeAnalysis.kt
+++ app/src/main/java/com/ihfazh/absensiqrcode/ui/qrcodecamera/QrCodeAnalysis.kt
@@ -1,8 +1,6 @@
 package com.ihfazh.absensiqrcode.ui.qrcodecamera
 
 import android.annotation.SuppressLint
-import android.graphics.*
-import android.media.Image
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
@@ -11,7 +9,6 @@ import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
-import java.io.ByteArrayOutputStream
 
 class QrCodeAnalysis(private val listener: QRCodeListener): ImageAnalysis.Analyzer {
     @RequiresApi(Build.VERSION_CODES.KITKAT)
@@ -19,43 +16,23 @@ class QrCodeAnalysis(private val listener: QRCodeListener): ImageAnalysis.Analyz
     override fun analyze(imageProxy:  ImageProxy) {
         val mediaImage = imageProxy.image
         if (mediaImage != null){
-            val image = InputImage.fromBitmap(croppedBitmap(mediaImage, imageProxy.cropRect), imageProxy.imageInfo.rotationDegrees)
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
 
             val scanner = BarcodeScanning.getClient()
             scanner.process(image)
-                .addOnSuccessListener { barcodes ->
-                    barcodes.forEach {
-                        listener.setResult(it)
+                    .addOnSuccessListener { barcodes ->
+                        barcodes.forEach {
+                            listener.setResult(it)
+                        }
                     }
-                }
-                .addOnFailureListener{
-                    Log.e(TAG, "analyze: errorrrrr $it", it )
-                }
-                .addOnSuccessListener {
-                    imageProxy.close()
-                }
+                    .addOnFailureListener{
+                        Log.e(TAG, "analyze: errorrrrr $it", it )
+                    }
+                    .addOnSuccessListener {
+                        imageProxy.close()
+                    }
+
         }
-    }
-
-    @SuppressLint("NewApi")
-    private fun croppedBitmap(mediaImage: Image, cropRect: Rect): Bitmap {
-        val yBuffer = mediaImage.planes[0].buffer // Y
-        val vuBuffer = mediaImage.planes[2].buffer // VU
-
-        val ySize = yBuffer.remaining()
-        val vuSize = vuBuffer.remaining()
-
-        val nv21 = ByteArray(ySize + vuSize)
-
-        yBuffer.get(nv21, 0, ySize)
-        vuBuffer.get(nv21, ySize, vuSize)
-
-        val yuvImage = YuvImage(nv21, ImageFormat.NV21, mediaImage.width, mediaImage.height, null)
-        val outputStream = ByteArrayOutputStream()
-        yuvImage.compressToJpeg(cropRect, 100, outputStream)
-        val imageBytes = outputStream.toByteArray()
-
-        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
     }
 
     interface QRCodeListener {
